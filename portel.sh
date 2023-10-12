c1f1d36fc1168d8be3a90b0152f6da07f0a761e4
Vishal Bhardwaj <35195272+SRDeveloperVishal@users.noreply.github.com>
2023-10-12 14:21:41 +0530

    Update portel.sh

--- portel.sh
+++ portel.sh
@@ -11,6 +11,7 @@
   sudo rm -rf /usr/local/bin/docker-compose
   
   #remove tutor file
+  sudo rm -rf "$(tutor config printroot)"
 
   sudo rm -rf /usr/local/bin/tutor
 
@@ -42,24 +43,12 @@
     # give permission to docker compose 
     sudo chmod +x /usr/local/bin/docker-compose
    
-   # clone tutor from github
-    git clone https://github.com/SRDeveloperVishal/tutor_13_clone.git
-
-    # change into tutor dic
-    mv tutor_13_clone tutor
-
-    # remove exisitng tutor 
-    sudo rm -rf .local/share/tutor
-
     # git clone tutor binary
     sudo curl -L "https://github.com/overhangio/tutor/releases/download/v13.1.5/tutor-$(uname -s)_$(uname -m)" -o /usr/local/bin/tutor
    
      # give permission to tutor user
     sudo chmod 0755 /usr/local/bin/tutor
     
-    # Now you can safely move or copy 'tutor/' to the destination directory
-    mv tutor .local/share/
-
     # take input from user
     echo "Enter LMS_HOST Name:"
     read LMS_HOST
@@ -78,13 +67,22 @@
 
    tutor config save --set CMS_HOST="studio.$LMS_HOST.rcmoocs.in"  \
    --set LMS_HOST="$LMS_HOST.rcmoocs.in"\
+   --set ENABLE_HTTPS=true \
    --set COURSE_EMAIL=$COURSE_EMAIL \
    --set SMTP_HOST="$SMTP_HOST" \
    --set SMTP_PORT=587 \
    --set SMTP_USE_TLS=true \
    --set SMTP_USERNAME="$SMTP_USERNAME" \
    --set SMTP_PASSWORD="$SMTP_PASSWORD" \
-   --set CONTACT_EMAIL="$CONTACT_EMAIL"
+   --set CONTACT_EMAIL="$CONTACT_EMAIL" \
+
+  # update lms and lms worker docker image
+  YAML_FILE=".local/share/tutor/env/local/docker-compose.yml"
+  NEW_IMAGE="7503444967/maple-edx-server:volume-final-api.0.6"
+
+ # Use sed to replace the image line for the 'lms' service
+  sed -i "/^ *lms:/,/^ *[^ ]/ s|image: docker.io/overhangio/openedx:13.1.5|image: $NEW_IMAGE|" "$YAML_FILE"  
+  sed -i "/^ *lms-worker:/,/^ *[^ ]/ s|image: docker.io/overhangio/openedx:13.1.5|image: $NEW_IMAGE|" "$YAML_FILE"  
 
    #tutor build 
    tutor local start -d
@@ -93,4 +91,11 @@
    tutor local init 
 
    # tutor settheme 
-   tutor local settheme edx-reborn-indigo
+   tutor local settheme edx-reborn-indigo 
+
+  # tutor restart
+  tutor local stop && tutor local start -d
+
+
+  echo "Work complete your $LMS_HOST port is ready"
+  echo "Thank you !!"
